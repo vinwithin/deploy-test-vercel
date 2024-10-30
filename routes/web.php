60ff5419e214ad08319da5bd4de700d208027e9a
dimaswisnuprabowo <kelvinadinata82@gmail.com>
2024-10-30 23:28:54 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\publikasiCOntroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [publikasiCOntroller::class, 'index']);
-Route::post('/', [publikasiCOntroller::class, 'store']);
+Route::post('/post', [publikasiCOntroller::class, 'store'])->name('post');
+Route::get('/test', function(){
+    return 'halo';
+} );
